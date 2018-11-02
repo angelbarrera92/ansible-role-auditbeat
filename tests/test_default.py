8e6f0d4ac8a11781eee820c998412f968b53ca80
Angel Barrera <angel.barrera@intelygenz.com>
2018-11-02 10:05:36 +0100

    testing not working yet

--- tests/test_default.py
+++ tests/test_default.py
@@ -11,24 +11,30 @@ def test_packages(host):
     assert host.package("auditbeat").is_installed
 
 
-def test_filebeat_file(host):
-    """Test the filebeat configuration file exists."""
+def test_auditbeat_file(host):
+    """Test the auditbeat configuration file exists."""
     assert host.file("/etc/auditbeat/auditbeat.yml").exists
 
 
-def test_filebeat_file_contains_config(host):
-    """Test the filebeat configuration file contains a current value."""
+def test_auditbeat_file_contains_config(host):
+    """Test the auditbeat configuration file contains a current value."""
     assert "scan_at_start" in host.file(
         "/etc/auditbeat/auditbeat.yml").content_string
 
 
-def test_filebeat_file_contains_logstash_config(host):
-    """Test the filebeat configuration file contains logstash default value."""
+def test_auditbeat_file_contains_logstash_config(host):
+    """Test the auditbeat configuration file contains logstash values."""
     assert "localhost:5044" in host.file(
         "/etc/auditbeat/auditbeat.yml").content_string
 
 
-def test_filebeat_file_contains_tags_config(host):
-    """Test the filebeat configuration file contains logstash default value."""
+def test_auditbeat_file_contains_tags_config(host):
+    """Test the auditbeat configuration file contains logstash values."""
     assert "tags: [\"auditbeat\"]" in host.file(
+        "/etc/auditbeat/auditbeat.yml").content_string
+
+
+def test_auditbeat_file_contains_logging_config(host):
+    """Test the auditbeat configuration file contains logging values."""
+    assert "debug" in host.file(
         "/etc/auditbeat/auditbeat.yml").content_string
